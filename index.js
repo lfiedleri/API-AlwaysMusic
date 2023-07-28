feec286c86e2f95c58eba55672584314663045fe
Linda Fiedler <lfiedler16@gmail.com>
2023-07-27 23:38:29 -0400

    se agregan read/insert

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const { 
+    getEstudiantes, 
+    insertEstudiantes
+} = require('./models/users');
+
+const PORT = process.env.PORT;
+
+// localhost:3000/
+app.get('/', async (req, res) => {
+    try {
+        const usuarios = await getEstudiantes();
+        console.log('estudiantes:>>', usuarios);
+        res.json(usuarios);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+
+// localhost:3000/create?rut=6.666.666-6&nombre=Diego+Pérez&curso=trompeta&nivel=3
+app.get('/create', async (req, res) => {
+    try {
+        rut = req.query.rut;
+        nombre = req.query.nombre;
+        curso = req.query.curso;
+        nivel = req.query.nivel;
+        const user = await insertEstudiantes(rut, nombre, curso, nivel);
+        console.log(`Estudiante ${nombre} agregado con éxito.`);
+        console.log(user);
+        res.json({
+            rut: user.rut,
+            nombre: user.nombre,
+            curso: user.curso,
+            nivel: user.nivel
+        });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// localhost:3000/updateRet?id=4&email=myemail@mail.com
+// app.get('/updateRet', async (req, res) => {
+//     try {
+//         id = req.query.id;
+//         email = req.query.email;
+//         const user = await modifyUserReturning(id, email);
+//         console.log(`Se ha modificado el usuario id ${id} con nuevo email ${email}`);
+//         res.json(user);
+//     } catch (error) {
+//         res.status(500).send(error.message);
+//     }
+// });
+
+// localhost:3000/deleteRet?id=6
+// app.get('/deleteRet', async (req, res) => {
+//     try {
+//         id = req.query.id;
+//         const user = await deleteUserReturning(id);
+//         console.log(`Se ha eliminado el usuario id ${id}`);
+//         res.json(user);
+//     } catch (error) {
+//         res.status(500).send(error.message);
+//     }
+// });
+
+// localhost:3000/read?id=2
+// app.get('/read', async (req, res) => {
+//     try {
+//         id = req.query.id;
+//         const user = await getUserById(id);
+//         res.json(user);
+//     } catch (error) {
+//         res.status(500).send(error.message);
+//     }
+// });
+
+app.listen(PORT, () => console.log(`Iniciando en puerto ${PORT}`));
--- models/users.js
+++ models/users.js
@@ -1,9 +1,11 @@
 const { pool } = require('../utils/getPool');
 
 const getEstudiantes = async () => {
+    const query = {
+        text: 'SELECT rut, nombre, curso, nivel FROM estudiante ORDER BY rut ASC'
+    }
     try {
-        const users = await pool.query('SELECT rut, nombre, curso, nivel FROM estudiante ORDER BY rut ASC');
-        console.log('users:', users);
+        const users = await pool.query(query);
         return users.rows;
     } catch (err) {
         console.log('err.message:', err.message);
@@ -12,22 +14,24 @@ const getEstudiantes = async () => {
     }
 }
 
-const insertEstudiantes = async (email, firstname, lastname, age) => {
+const insertEstudiantes = async (rut, nombre, curso, nivel) => {
     const query = {
-        text: 'INSERT INTO users (email, firstname, lastname, age) VALUES ($1, $2, $3, $4)',
-        values: [email, firstname, lastname, age]
+        text: 'INSERT INTO estudiante (rut, nombre, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *',
+        values: [rut, nombre, curso, nivel]
     }
     try {
         const res = await pool.query(query);
-        console.log(`Se ha agregado el usuario ${firstname}`);
-        console.log('res:', res);
+        //console.log(`Se ha agregado el estudiante ${nombre}`);
+        //console.log('res.rows[0]:', res.rows[0]);
+        return res.rows[0];
     } catch (error) {
         console.log('err.message:', err.message);
         console.log('err.stack:', err.stack);
+        return [];
     }
 }
 
 module.exports = {
-    getEstudiantes,
+    getEstudiantes, 
     insertEstudiantes
 }
