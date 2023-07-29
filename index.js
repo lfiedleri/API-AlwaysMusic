5b148b1a75f27b1082c505cd7f66c64cd92b6317
Linda Fiedler <lfiedler16@gmail.com>
2023-07-28 22:30:45 -0400

    Se agregan funcionalidades CRUD

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 require('dotenv').config();
 
 const { 
-    getEstudiantes, 
-    insertEstudiantes
+    getStudents, 
+    insertEstudiantes,
+    updateStudents,
+    deleteStudents, 
+    getStudentByRut,
+    getStudentsArray
 } = require('./models/users');
 
 const PORT = process.env.PORT;
@@ -12,9 +16,19 @@ const PORT = process.env.PORT;
 // localhost:3000/
 app.get('/', async (req, res) => {
     try {
-        const usuarios = await getEstudiantes();
-        console.log('estudiantes:>>', usuarios);
-        res.json(usuarios);
+        const estudiantes = await getStudents();
+        console.log('estudiantes:>>', estudiantes);
+        res.json(estudiantes);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// localhost:3000/array
+app.get('/array', async (req, res) => {
+    try {
+        const estudiantes = await getStudentsArray();
+        res.json(estudiantes);
     } catch (error) {
         res.status(500).send(error.message);
     }
@@ -42,43 +56,44 @@ app.get('/create', async (req, res) => {
     }
 });
 
-//localhost:3000/updateRet?rut=7.777.777-7&nombre=Francisco+Vergara&curso=ballet&nivel5
-app.get('/updateRet', async (req, res) => {
+//localhost:3000/update?rut=6.666.666-6&curso=ballet
+app.get('/update', async (req, res) => {
     try {
         rut = req.query.rut;
-        nombre = req.query.nombre;
         curso = req.query.curso;
-        nivel = req.query.nivel;
-        const user = await modifyStudentReturning(rut, nombre, curso, nivel);
-        console.log(`Se ha modificado el usuario rut ${rut}, 
-        ${nombre}, para el curso ${ballet} en el nivel ${nivel}`);
+        const user = await updateStudents(rut, curso);
+        console.log(`Se ha modificado el estudiante rut ${rut} con nuevo curso ${curso}`);
+        console.log(user);
         res.json(user);
     } catch (error) {
         res.status(500).send(error.message);
     }
 });
 
-// localhost:3000/deleteRet?id=6
-// app.get('/deleteRet', async (req, res) => {
-//     try {
-//         id = req.query.id;
-//         const user = await deleteUserReturning(id);
-//         console.log(`Se ha eliminado el usuario id ${id}`);
-//         res.json(user);
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// });
+//localhost:3000/delete?rut=6.666.666-6
+app.get('/delete', async (req, res) => {
+    try {
+        rut = req.query.rut;
+        const user = await deleteStudents(rut);
+        console.log(`Se ha eliminado el usuario rut ${rut}`);
+        console.log(user);
+        res.json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
-// localhost:3000/read?id=2
-// app.get('/read', async (req, res) => {
-//     try {
-//         id = req.query.id;
-//         const user = await getUserById(id);
-//         res.json(user);
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// });
+//localhost:3000/read?rut=22.222.222-2
+app.get('/read', async (req, res) => {
+    try {
+        rut = req.query.rut;
+        const user = await getStudentByRut(rut);
+        console.log(`Estudiante encontrado rut ${rut}`);
+        console.log(user);
+        res.json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 app.listen(PORT, () => console.log(`Iniciando en puerto ${PORT}`));
--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const { pool } = require('../utils/getPool');
 
-const getEstudiantes = async () => {
+const getStudents = async () => {
     const query = {
         text: 'SELECT rut, nombre, curso, nivel FROM estudiante ORDER BY rut ASC'
     }
@@ -12,26 +12,96 @@ const getEstudiantes = async () => {
         console.log('err.stack:', err.stack);
         return [];
     }
-}
+};
 
-const insertEstudiantes = async (rut, nombre, curso, nivel) => {
+const insertStudents = async (rut, nombre, curso, nivel) => {
     const query = {
         text: 'INSERT INTO estudiante (rut, nombre, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *',
         values: [rut, nombre, curso, nivel]
     }
     try {
         const res = await pool.query(query);
-        //console.log(`Se ha agregado el estudiante ${nombre}`);
-        //console.log('res.rows[0]:', res.rows[0]);
         return res.rows[0];
     } catch (error) {
         console.log('err.message:', err.message);
         console.log('err.stack:', err.stack);
         return [];
     }
-}
+};
+
+const updateStudents = async (rut, curso) => {
+    const query = {
+        text: 'UPDATE estudiante SET curso = $2 WHERE rut = $1 RETURNING *',
+        values: [rut, curso]
+    }
+    try {
+        const res = await pool.query(query);
+        return res.rows[0];
+    } catch (error) {
+        console.log('err.message:', err.message);
+        console.log('err.stack:', err.stack);
+        return [];
+    }
+};
+
+const deleteStudents = async (rut) => {
+    const query = {
+        text: 'DELETE FROM estudiante WHERE rut = $1 RETURNING *',
+        values: [rut]
+    }
+    try {
+        const res = await pool.query(query);
+        return res.rows[0] || [];
+    } catch (error) {
+        console.log('err.message:', err.message);
+        console.log('err.stack:', err.stack);
+        return [];
+    }
+};
+
+const getStudentByRut = async (rut) => {
+    const query = {
+        text: 'SELECT rut, nombre, curso, nivel FROM estudiante WHERE rut = $1',
+        values: [rut]
+    }
+    try {
+        const res = await pool.query(query);
+        return res.rows[0] || {};
+    } catch (err) {
+        console.log('err.message:', err.message);
+        console.log('err.stack:', err.stack);
+        return {};
+    }
+};
+
+const getStudentsArray = async () => {
+    const query = {
+        text: "SELECT rut, nombre, curso, nivel FROM estudiante ORDER BY rut ASC",
+        rowMode: "array",
+    };
+    try {
+        const client = await pool.connect();
+        const result = await client.query(query);
+        if (result.rows.length) {
+            console.log("Estudiantes encontrados:", result.rowCount);
+            for (let row of result.rows) {
+                console.log(row);
+            }
+            client.release();
+            return result.rows;
+        }
+    } catch (err) {
+        console.log("err.message:", err.message);
+        console.log("err.stack:", err.stack);
+        return [];
+    }
+};
 
 module.exports = {
-    getEstudiantes, 
-    insertEstudiantes
+    getStudents,
+    insertStudents,
+    updateStudents,
+    deleteStudents,
+    getStudentByRut,
+    getStudentsArray
 }
